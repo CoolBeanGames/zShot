81bfd91c77056db6d42a29a52eb01fac4af3774d
CoolBeanGames <CallMeHaptic@yahoo.com>
2026-09-07 04:21:52 -0500

    Fix release packaging - copy updater to builds/release before zip

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #pragma comment(lib, "ole32.lib")
 #pragma comment(lib, "wininet.lib")
 
-#define CURRENT_BUILD_NUMBER 19
+#define CURRENT_BUILD_NUMBER 20
 
 #include <thread>
 #include <urlmon.h>
