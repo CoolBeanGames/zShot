4c94fc51613122f3c94a2e89f393b97d0a218dee
CoolBeanGames <CallMeHaptic@yahoo.com>
2026-09-07 03:45:12 -0500

    Bump build and manifest versions to 12

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #pragma comment(lib, "ole32.lib")
 #pragma comment(lib, "wininet.lib")
 
-#define CURRENT_BUILD_NUMBER 10
+#define CURRENT_BUILD_NUMBER 12
 
 #include <thread>
 #include <urlmon.h>
